562018ae461f95c6e1a2ef38fdf4fd045409058a
Iwan de Kok <iwan.de.kok@outlook.com>
2021-01-12 15:05:11 +0100

    Day 21 added

--- PycharmProjects/pythonProject/100_days_of_code/day 20_21/main.py
+++ PycharmProjects/pythonProject/100_days_of_code/day 20_21/main.py
@@ -1,5 +1,7 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -8,8 +10,9 @@ screen.bgcolor("black")
 screen.title("Snake game")
 screen.tracer(0)
 
-# 1. Create snake body at the start position
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -17,10 +20,27 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-# TODO: 2. Move snake
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend_snake()
+        scoreboard.increase_scoreboard()
+
+    # Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail.
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
---
+++ PycharmProjects/pythonProject/beurskoersen/main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+# df = pd.read_html("https://www.beleggen.nl/Aandeel-Koers/613007/ADYEN-NV.aspx")
+
+df = pd.read_csv("https://www.beleggen.nl/Aandeel-Koers/613007/ADYEN-NV.aspx")
+df.head()
