da9df1bc44df1f5ed06fb6ffe8d996f6b5f92118
Iwan de Kok <iwan.de.kok@outlook.com>
2021-01-31 15:26:40 +0100

    Day 29 completed

--- PycharmProjects/pythonProject/100_days_of_code/day 29 password-manager-start/main.py
+++ PycharmProjects/pythonProject/100_days_of_code/day 29 password-manager-start/main.py
@@ -1,7 +1,29 @@
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+               'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_entry.delete(0, END)
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_numbers + password_symbols
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -10,16 +32,18 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
 
-    is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \n "
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showwarning(title="Oops", message="Please don't leave any fields empty!")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \n "
                                                           f"Email: {email} \n "
                                                           f"Password: {password} \n\n"
                                                           f"Is it ok to save?")
-
-    if is_ok:
-        with open("passwords.txt", "a") as data_file:
-            data_file.write(f"{website} | {email} | {password}\n")
-            website_entry.delete(0, END)
-            password_entry.delete(0, END)
+        if is_ok:
+            with open("passwords.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -51,32 +75,9 @@ password_entry = Entry(width=32)
 password_entry.grid(row=3, column=1)
 
 # Buttons
-generate_button = Button(text="Generate Password", width=15)
+generate_button = Button(text="Generate Password", width=15, command=generate_password)
 generate_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=44, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
-
-
-
-
-# timer_text = canvas.create_text(100, 130, text="00:00", fill="White", font=(FONT_NAME, 27, "bold"))
-
-
-
-# label_title = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50))
-# label_title.grid(column=1, row=0)
-#
-
-#
-#
-# button_start = Button(text="Start", width=7, command=start_timer)
-# button_start.grid(column=0, row=3)
-#
-# button_reset = Button(text="Reset", width=7, command=reset_timer)
-# button_reset.grid(column=3, row=3)
-#
-# label_check_marks = Label(fg=GREEN, bg=YELLOW)
-# label_check_marks.grid(column=1, row=4)
-#
 window.mainloop()
