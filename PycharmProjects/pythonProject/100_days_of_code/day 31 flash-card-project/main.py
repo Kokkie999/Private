2638be9bb1c228a92326c264ca6e0be4a78baf60
Iwan de Kok <iwan.de.kok@outlook.com>
2021-02-01 21:11:11 +0100

    Day 31 completed

--- PycharmProjects/pythonProject/100_days_of_code/day 31 flash-card-project/main.py
+++ PycharmProjects/pythonProject/100_days_of_code/day 31 flash-card-project/main.py
@@ -6,10 +6,16 @@ import math
 from tkinter import messagebox
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
 current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
 def flip_card():
@@ -25,7 +31,14 @@ def next_card():
     canvas.itemconfig(card_background, image=card_front_img)
     canvas.itemconfig(card_title, text="French", fill="Black")
     canvas.itemconfig(card_word, text=current_card["French"], fill="Black")
-    window.after(3000, func=flip_card)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
 
 
 window = Tk()
@@ -49,7 +62,7 @@ cross_img = PhotoImage(file="images/wrong.png")
 unknown_button = Button(image=cross_img, highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=0)
 check_img = PhotoImage(file="images/right.png")
-known_button = Button(image=check_img, highlightthickness=0, command=next_card)
+known_button = Button(image=check_img, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
 next_card()
