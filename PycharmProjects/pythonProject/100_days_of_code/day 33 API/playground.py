6055f853ff16b64e2ae0b307105f631b8f6cdb66
Iwan de Kok <iwan.de.kok@outlook.com>
2021-02-06 19:31:25 +0100

    Day 33 added

---
+++ PycharmProjects/pythonProject/100_days_of_code/day 33 API/playground.py
@@ -0,0 +1,40 @@
+import requests
+from datetime import datetime
+
+MY_LAT = 52.167137
+MY_LONG = 4.634807
+
+# ISS_LOCATION
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+#
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+# iss_position = (longitude, latitude)
+#
+# print(iss_position)
+
+# SUNSET & SUNRISE
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+time_now = datetime.now()
+
+print(sunrise)
+print(sunset)
+print(time_now.hour)
+
+
+
