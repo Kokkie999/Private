57c2d31fed37f1b0ca230b3168b9c43f2172d382
Iwan de Kok <iwan.de.kok@outlook.com>
2021-02-11 20:31:31 +0100

    Day 35 added

--- PycharmProjects/pythonProject/100_days_of_code/day 33 API/playground.py
+++ PycharmProjects/pythonProject/100_days_of_code/day 33 API/playground.py
@@ -1,40 +1,53 @@
 import requests
-from datetime import datetime
-
-MY_LAT = 52.167137
-MY_LONG = 4.634807
-
-# ISS_LOCATION
-# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# from datetime import datetime
+#
+# MY_LAT = 52.167137
+# MY_LONG = 4.634807
+#
+# # ISS_LOCATION
+# # response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# # response.raise_for_status()
+# #
+# # data = response.json()
+# #
+# # longitude = data["iss_position"]["longitude"]
+# # latitude = data["iss_position"]["latitude"]
+# #
+# # iss_position = (longitude, latitude)
+# #
+# # print(iss_position)
+#
+# # SUNSET & SUNRISE
+# parameters = {
+#     "lat": MY_LAT,
+#     "lng": MY_LONG,
+#     "formatted": 0,
+# }
+#
+# response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
 # response.raise_for_status()
-#
 # data = response.json()
+# sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+# sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 #
-# longitude = data["iss_position"]["longitude"]
-# latitude = data["iss_position"]["latitude"]
+# time_now = datetime.now()
 #
-# iss_position = (longitude, latitude)
-#
-# print(iss_position)
+# print(sunrise)
+# print(sunset)
+# print(time_now.hour)
 
-# SUNSET & SUNRISE
 parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
+    "where": "1=1",
+    #"outFields": "sgisid,vopcode",
+    #"returnCentroid": "true",
+    "f": "json",
+    "token": "vYNaREh6jNSfkvY819tskCS4Rnhf6a0Qnev6S3d0rs2QsLQisuqFDA1QEl32l8qaTdeeA4Uo9UFlRIr1xn_x-O1nkqC7vGYIOrnq6VlFcX7HP-yOW1Eu47IrVWyKLQ1OkV0R0wcHSwd27IxI937tlwtxQj6o97-BJUIEO6Qqnt8oY95iyse6YrNeoRkX-40y1KeAG-TUnIZrs7Um0v3squY0Yw1FVZEpkwGK7EYylT84aRV4booLlSusZak1qZoa",
 }
 
-response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response = requests.get(url="https://arcgis.schiphol.nl/arcgishosted/rest/services/Hosted/Vliegtuig_Opstel_Plaats_%28VOP%29/FeatureServer/0/query", params=parameters)
 response.raise_for_status()
+
 data = response.json()
-sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
-sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
-time_now = datetime.now()
-
-print(sunrise)
-print(sunset)
-print(time_now.hour)
-
-
+print(data)
 
---
+++ PycharmProjects/pythonProject/100_days_of_code/day 35 SMS & API/main.py
@@ -0,0 +1,49 @@
+import requests
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+
+OWM_endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+OWM_apiKey = "420b76cf820dd44a2ee56a31fbf24e3f"
+#OWM_apiKey = os.environ.get("OWM_API_KEY")
+account_sid = "ACded0659b3cf8aeb9efbac4621f742e5d"
+auth_token = "afb7ed24d57a0208bf11bf095055797a"
+
+print(OWM_apiKey)
+
+weather_params = {
+    # "lat": "52.167137",
+    # "lon": "4.634807",
+    "lat": "49.542801",
+    "lon": "28.036854",
+    "appid": OWM_apiKey,
+    "exclude": "current,minutely,daily"
+}
+
+response = requests.get(OWM_endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
+
+
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+    client = Client(account_sid, auth_token, http_client=proxy_client)
+    #client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an umbrella.",
+        from_="+12342353373",
+        to="+31615633707"
+    )
+
+    print(message.status)
