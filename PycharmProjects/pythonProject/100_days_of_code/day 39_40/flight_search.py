2f521fc1c57c3111d6a565eb95ad52bebfe6202b
Iwan de Kok <iwan.de.kok@outlook.com>
2021-02-21 20:21:39 +0100

    Day 39-40 updated

--- PycharmProjects/pythonProject/100_days_of_code/day 39_40/flight_search.py
+++ PycharmProjects/pythonProject/100_days_of_code/day 39_40/flight_search.py
@@ -1,3 +1,14 @@
+import json
+
+with open("C:/Users/iwand/OneDrive/api_key.json") as data_file:
+    api_data = json.load(data_file)
+
+ENDPOINT = api_data['Tequila']['endpoint']
+API_KEY = api_data['Tequila']['api_key']
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+
+    def get_destination_code(self, city_name):
+        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
+        code = "TESTING"
+        return code
