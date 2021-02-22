196c464ea0051671138303e18f8fa0cf7166fefd
Iwan de Kok <iwan.de.kok@outlook.com>
2021-02-22 21:09:45 +0100

    API manager updated

--- PycharmProjects/pythonProject/API_info/main.py
+++ PycharmProjects/pythonProject/API_info/main.py
@@ -10,6 +10,8 @@ def save():
     api_id = api_id_entry.get()
     api_key = api_key_entry.get()
     token = token_entry.get()
+    phone_number = phone_number_entry.get()
+    my_number = my_phone_number_entry.get()
 
     new_data = {
         api_name: {
@@ -17,6 +19,8 @@ def save():
             "api_id": api_id,
             "api_key": api_key,
             "token": token,
+            "phone_number": phone_number,
+            "my_phone_number": my_number,
         }
     }
 
@@ -43,6 +47,8 @@ def save():
             api_id_entry.delete(0, END)
             api_key_entry.delete(0, END)
             token_entry.delete(0, END)
+            phone_number_entry.delete(0, END)
+            my_phone_number_entry.delete(0, END)
 
 
 # ------------------------- FIND API-DATA ----------------------------- #
@@ -59,7 +65,9 @@ def find():
             api_id = data[api_name]["api_id"]
             api_key = data[api_name]["api_key"]
             token = data[api_name]["token"]
-            messagebox.showinfo(title=api_name, message=f"Endpoint: {endpoint}\nAPI ID: {api_id}\nAPI key: {api_key}\nToken: {token}")
+            phone_number = data[api_name]["phone_number"]
+            my_phone_number = data[api_name]["my_phone_number"]
+            messagebox.showinfo(title=api_name, message=f"Endpoint: {endpoint}\nAPI ID: {api_id}\nAPI key: {api_key}\nToken: {token}\nPhone number: {phone_number}\nMy phone number: {my_phone_number}")
         else:
             messagebox.showwarning(title="Error", message=f"No details for {website} exists")
 
@@ -77,6 +85,8 @@ endpoint_label = Label(text="Endpoint:", justify=LEFT).grid(sticky=W, row=2, col
 api_id_label = Label(text="API ID:", justify=LEFT).grid(sticky=W, row=3, column=0)
 api_key_label = Label(text="API key:", justify=LEFT).grid(sticky=W, row=4, column=0)
 api_token_label = Label(text="Token:", justify=LEFT).grid(sticky=W, row=5, column=0)
+phone_number_label = Label(text="Phone number:", justify=LEFT).grid(sticky=W, row=6, column=0)
+my_phone_number_label = Label(text="My phone number:", justify=LEFT).grid(sticky=W, row=7, column=0)
 
 # Entries
 api_name_entry = Entry(width=35, justify=LEFT)
@@ -89,12 +99,16 @@ api_key_entry = Entry(width=60, justify=LEFT)
 api_key_entry.grid(sticky=W, row=4, column=1, columnspan=2)
 token_entry = Entry(width=60, justify=LEFT)
 token_entry.grid(sticky=W, row=5, column=1, columnspan=2)
+phone_number_entry = Entry(width=60, justify=LEFT)
+phone_number_entry.grid(sticky=W, row=6, column=1, columnspan=2)
+my_phone_number_entry = Entry(width=60, justify=LEFT)
+my_phone_number_entry.grid(sticky=W, row=7, column=1, columnspan=2)
 
 
 # Buttons
 search_button = Button(text="Zoeken", width=18, justify=RIGHT, command=find)
 search_button.grid(sticky=W, row=1, column=2)
 add_button = Button(text="Toevoegen", width=18, justify=LEFT, command=save)
-add_button.grid(sticky=W, row=6, column=1)
+add_button.grid(sticky=W, row=8, column=1)
 
 window.mainloop()
